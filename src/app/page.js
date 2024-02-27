fbc85f8c7c7d4796e85b4e686e19e3702380a4b9
Sheleoni <sheleoni@gmail.com>
2024-02-28 00:32:40 +0900

    feat: use ButtonContainer component to extract client side code

--- src/app/page.js
+++ src/app/page.js
@@ -1,21 +1,18 @@
-'use client'
 import styles from "./page.module.css";
 import UnicornImage from "./Uni-code_Unicorn_.png"
 import Image from "next/image";
-const postSomeData = async () => {
-    const res = await fetch('/api/sendData/', {method: 'POST'});
-    window.alert("Data posted!")
-}
+import {ButtonContainer} from "@/app/components/ButtonContainer";
+
 
 export default function Home() {
   return (
     <main className={styles.main}>
-        <button onClick={postSomeData}>
+        <ButtonContainer>
             <Image src={UnicornImage} alt={"Image of a unicorn typing on a laptop."} width={150} height={150} />
             <p>
                 Submit (POST data)
             </p>
-        </button>
+        </ButtonContainer>
     </main>
   );
 }
