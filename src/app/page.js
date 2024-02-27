87afe17e35d565ca94168f3903142f55b4cf06f2
Sheleoni <sheleoni@gmail.com>
2024-02-28 01:01:48 +0900

    feat: experiment without image

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 'use client'
 import styles from "./page.module.css";
-import UnicornImage from "./Uni-code_Unicorn_.png"
-import Image from "next/image";
+// import UnicornImage from "./Uni-code_Unicorn_.png"
+// import Image from "next/image";
 const postSomeData = async () => {
     const res = await fetch('/api/sendData/', {method: 'POST'});
     window.alert("Data posted!")
@@ -11,7 +11,7 @@ export default function Home() {
   return (
     <main className={styles.main}>
         <button onClick={postSomeData}>
-            <Image src={UnicornImage} alt={"Image of a unicorn typing on a laptop."} width={150} height={150} />
+            {/*<Image src={UnicornImage} alt={"Image of a unicorn typing on a laptop."} width={150} height={150} />*/}
             <p>
                 Submit (POST data)
             </p>
