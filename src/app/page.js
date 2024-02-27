8e822d05c2380f8ffd23034d8a8e2380129a8d80
Sheleoni <sheleoni@gmail.com>
2024-02-28 00:15:51 +0900

    feat: update layout, add image

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,20 @@
 'use client'
 import styles from "./page.module.css";
+import UnicornImage from "./Uni-code_Unicorn_.png"
+import Image from "next/image";
 const postSomeData = async () => {
     const res = await fetch('/api/sendData/', {method: 'POST'});
+    window.alert("Data posted!")
 }
 
 export default function Home() {
   return (
     <main className={styles.main}>
         <button onClick={postSomeData}>
-            POST some data
+            <Image src={UnicornImage} alt={"Image of a unicorn typing on a laptop."} width={150} height={150} />
+            <p>
+                Submit (POST data)
+            </p>
         </button>
     </main>
   );
