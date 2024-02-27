44faa026b39c830e861e84e13c4fc8065b55986a
Sheleoni <sheleoni@gmail.com>
2024-02-28 01:22:23 +0900

    feat: add extra image

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 'use client'
 import styles from "./page.module.css";
-import UnicornImage from "./Uni-code_Unicorn_.png"
+import UnicornImage from "./Uni-code_Unicorn_.png";
+import UnicornInTheSunImage from "./Unicorn_In_The_Sun.png";
 import Image from "next/image";
 const postSomeData = async () => {
     const res = await fetch('/api/sendData/', {method: 'POST'});
@@ -12,6 +13,9 @@ export default function Home() {
     <main className={styles.main}>
         <button onClick={postSomeData}>
             <Image src={UnicornImage} alt={"Image of a unicorn typing on a laptop."} width={150} height={150} />
+            <p>
+                <Image src={UnicornInTheSunImage} alt={"Image of a unicorn at the beach."} width={300} height={300} />
+            </p>
             <p>
                 Submit (POST data)
             </p>
