5c77a1b8465183aad9a98bb4aa590ba8028ae3d4
Sheleoni <sheleoni@gmail.com>
2024-02-29 02:23:40 +0900

    feat: remove images

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,4 @@
 import styles from "./page.module.css";
-import UnicornImage from "./Uni-code_Unicorn_.png";
-import UnicornInTheSunImage from "./Unicorn_In_The_Sun.png";
-import Image from "next/image";
 import {ButtonContainer} from "@/app/components/ButtonContainer";
 
 
@@ -9,13 +6,6 @@ export default function Home() {
   return (
     <main className={styles.main}>
         <ButtonContainer>
-            <Image src={UnicornImage} alt={"Image of a unicorn typing on a laptop."} width={150} height={150}/>
-            <p>
-                <Image src={UnicornInTheSunImage} alt={"Image of a unicorn at the beach."} width={300} height={300}/>
-            </p>
-            <p>
-                <Image src={UnicornInTheSunImage} alt={"Image of a unicorn at the beach."} width={300} height={300} />
-            </p>
             <p>
                 Submit (POST data)
             </p>
